b2f8013aff6fc7a49c9278e800f3f28b1d43f367
Risindu Chiranthana Madhushan <30834066+Risindu@users.noreply.github.com>
2023-12-02 12:04:19 +0530

    Enabled User Signup Through Confirmation Email And Google Less Secure Apps

---
+++ DiaSense/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'chiranthanawijewardhana@gmail.com'
+EMAIL_HOST_PASSWORD = '0632223578'
+EMAIL_PORT = 587
--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import  messages
+from DiaSense import settings
+
+
+
 # Create your views here.
 def home(request):
     return render(request,"app/index.html")
@@ -17,13 +21,33 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+        if User.objects.filter(username=username):
+            messages.error(request,"Username already exist! Tyr another username")
+            return redirect('home')
+
+        if len(username)>20:
+            messages.error(request,"Username must be 10 characters")
+
+        if pass1 != pass2:
+            messages.error(request,"Password didn't match!")
+
+        if not username.isalnum():
+            messages.error(request,"Username must be Alpha numeric")
+            return redirect('home')
+
         myuser = User.objects.create_user(username,email,pass1)
         myuser.first_name = fname
         myuser.last_name = lname
 
         myuser.save()
-        messages.success(request, "Your account has been successfully created")
+        messages.success(request, "Your account has been successfully created We have sent you a confirmation email, please confirm your email to activate your account")
 
+        #welcome email
+        subject = "Welcome to DiaSense- Diabetes Predictor"
+        message = "Hello"+myuser.first_name+"! \n"+"Welcome to DiaSense! \n"+ "Thank you for visiting our website\n Please click on confirmation email to confirm your email address. \n\n Thank You! \n DiaSense(Team)"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject,message,from_email,to_list,fail_silently = True)
         return redirect('signin')
 
     return render(request,"app/signup.html")
