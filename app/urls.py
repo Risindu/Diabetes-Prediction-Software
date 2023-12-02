04b3141df5bbf4be110fb60655d170a63896a04c
Risindu Chiranthana Madhushan <30834066+Risindu@users.noreply.github.com>
2023-12-02 11:06:07 +0530

    Login and Signup
    Initial commit (Only Signup and Log in)

--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('app.urls')),
+    path('',views.home,name="home"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('signout', views.signout, name="signout"),
+
+
 ]
